0df8850f8e1497d0e46754e31eda2e2cb88d2910
Karthik <karthik@imorph.com>
2010-02-02 14:11:57 +0530

    Chore: Used smarty template engine to render IBIS page

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 copy settings\ C:\wamp\www\wiki\
 copy css\main.css C:\wamp\www\wiki\skins\monobook
-copy skin\ C:\wamp\www\wiki\skins\
-copy extension\IBISMediaWiki C:\wamp\www\wiki\extensions\IBISMediaWiki
+xcopy /S /E /Q /Y /I ibis_includes C:\wamp\www\wiki\ibis_includes
+xcopy /S /E /Y extension\IBISMediaWiki C:\wamp\www\wiki\extensions\IBISMediaWiki 
