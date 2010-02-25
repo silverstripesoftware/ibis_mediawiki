a00daa3b3b96b7ac55d64d731f3c948c4880f7eb
Karthik <karthik@imorph.com>
2010-02-25 17:30:20 +0530

    Used IBISIncludes.php file and centralized all the lib includes

---
+++ extension/IBISMediaWiki/IBISIncludes.php
@@ -0,0 +1,26 @@
+<?php
+/*******************************************************************************
+	Code contributed to the Bloomer Project
+    Copyright (C) 2010 iMorph Inc.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as 
+	published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
+// Smarty 
+require_once("$IP/extensions/IBISMediaWiki/includes/smarty/Smarty.class.php");
+// FCK Editor
+include_once("$IP/extensions/IBISMediaWiki/includes/fckeditor/fckeditor.php");
+// HTML Cleaner - htmLawd
+require_once("$IP/extensions/IBISMediaWiki/includes/htmlcleaner/htmLawed.php");
+// YAML Lib - spyc
+require_once("$IP/extensions/IBISMediaWiki/includes/spyc/spyc.php");
+?>
